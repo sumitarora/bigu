4da40c228fee348bb64c1b6057493209cb7f8e09
rubyverma <rubyverma90@ymail.com>
2013-07-30 19:28:38 -0400

    task 2 complete

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -22,6 +22,22 @@ $log.info("Testing Controller");
 angular.module('bigu.controllers').controller('GridCtrl', function ($scope, $log) {
 
 $log.info("Grid Controller");
+$scope.showGrid = true;
+
+$scope.clickAdd = function(){
+	$log.info("Added");
+	$scope.showGrid = false;
+};
+
+$scope.clickSave = function(){
+	$log.info("Save Working");
+	$scope.showGrid = true;
+};
+
+$scope.clickCancel = function(){
+	$log.info("Cancel Working");
+	$scope.showGrid = true;
+};
 
 });
 angular.module('bigu.controllers').controller('HelloCtrl', function ($scope, $log) {
