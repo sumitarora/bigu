78f5d0deba7df3653d33d91025295de2a6ad8f28
rubyverma <rubyverma90@ymail.com>
2013-07-31 17:36:41 -0400

    task 3 complete

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -26,6 +26,16 @@ $scope.clickCancel = function(){
 	$scope.showGrid = true;
 };
 
+$scope.courses = [
+{title:'T1',category:'C1', website: 'W1', type: 'Type1', viewCount: 'VC1', rating: 'R1'},
+{title:'T2',category:'C2', website: 'W2', type: 'Type2', viewCount: 'VC2', rating: 'R2'}
+];
+
+$scope.addCourse = function(){
+	$log.info("Add Course Working");
+	$scope.courses.push({title:'Title',category:'Category', website: 'Website', type: 'Type', viewCount: 'View Count', rating: 'Rating'});
+};
+
 });
 angular.module('bigu.controllers').controller('SampleCtrl', function ($scope, $log) {
 
--- src/js/controllers/courseseries_controller.js
+++ src/js/controllers/courseseries_controller.js
@@ -18,4 +18,14 @@ $scope.clickCancel = function(){
 	$scope.showGrid = true;
 };
 
+$scope.courses = [
+{title:'T1',category:'C1', website: 'W1', type: 'Type1', viewCount: 'VC1', rating: 'R1'},
+{title:'T2',category:'C2', website: 'W2', type: 'Type2', viewCount: 'VC2', rating: 'R2'}
+];
+
+$scope.addCourse = function(){
+	$log.info("Add Course Working");
+	$scope.courses.push({title:'Title',category:'Category', website: 'Website', type: 'Type', viewCount: 'View Count', rating: 'Rating'});
+};
+
 });
