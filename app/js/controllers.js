5bee27ccb7b19a0e46c4e5be8403977b86953683
rubyverma <rubyverma90@ymail.com>
2013-08-02 16:19:14 -0400

    task 5 incomplete

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -14,7 +14,22 @@ $scope.showGrid = true;
 $scope.clickAdd = function(){
 	$log.info("Added");
 	$scope.showGrid = false;
+	$scope.defaultValuesFunction();
 };
+
+$scope.clickEdit = function(course){
+	$log.info(course);
+	$scope.course = angular.copy(course);
+	$scope.showGrid = false;
+	$scope.index = $scope.courses.indexOf(course);
+};
+
+$scope.clickDelete = function(course){
+	$log.info(course);
+	var index = $scope.courses.indexOf(course);
+	$scope.courses.splice(index,1);
+};
+
 
 $scope.defaultValuesFunction =function(){
 	$scope.course = {type: 'Series', viewCount:'1200', rating: '4.9'};
@@ -28,8 +43,14 @@ $scope.clickSave = function(){
 	$log.info("Save Working");
 	$scope.showGrid = true;
 	$log.info($scope.course);
-	$scope.courses.push($scope.course);
-	$scope.defaultValuesFunction();
+	if($scope.index > -1){
+		$scope.defaultValuesFunction();
+		$log.info($scope.course);
+		$scope.index = -1;
+	}
+	else{
+			$scope.courses.push($scope.course);
+	}
 };
 
 $scope.clickCancel = function(){
@@ -37,7 +58,7 @@ $scope.clickCancel = function(){
 	$scope.showGrid = true;
 };
 
-/*
+
 $scope.friends = [];
 
 $scope.friend = {};
@@ -46,7 +67,22 @@ $scope.add = function(){
 	$log.info($scope.friend);
 	$scope.friends.push($scope.friend);
 	$scope.friend = {};
-};*/
+};
+
+
+
+ $scope.deleteClicked = function(friend) {
+	$log.info(friend);
+	var index = $scope.friends.indexOf(friend);
+	$scope.friends.splice(index, 1);
+  };
+
+$scope.editClicked = function(friend) {
+	$log.info(friend);
+	friend.name = $scope.friend.name;
+	friend.lives = $scope.friend.lives;
+  };
+
 
 
 });
--- src/js/controllers/courseseries_controller.js
+++ src/js/controllers/courseseries_controller.js
@@ -6,7 +6,22 @@ $scope.showGrid = true;
 $scope.clickAdd = function(){
 	$log.info("Added");
 	$scope.showGrid = false;
+	$scope.defaultValuesFunction();
 };
+
+$scope.clickEdit = function(course){
+	$log.info(course);
+	$scope.course = angular.copy(course);
+	$scope.showGrid = false;
+	$scope.index = $scope.courses.indexOf(course);
+};
+
+$scope.clickDelete = function(course){
+	$log.info(course);
+	var index = $scope.courses.indexOf(course);
+	$scope.courses.splice(index,1);
+};
+
 
 $scope.defaultValuesFunction =function(){
 	$scope.course = {type: 'Series', viewCount:'1200', rating: '4.9'};
@@ -20,8 +35,14 @@ $scope.clickSave = function(){
 	$log.info("Save Working");
 	$scope.showGrid = true;
 	$log.info($scope.course);
-	$scope.courses.push($scope.course);
-	$scope.defaultValuesFunction();
+	if($scope.index > -1){
+		$scope.defaultValuesFunction();
+		$log.info($scope.course);
+		$scope.index = -1;
+	}
+	else{
+			$scope.courses.push($scope.course);
+	}
 };
 
 $scope.clickCancel = function(){
@@ -29,7 +50,7 @@ $scope.clickCancel = function(){
 	$scope.showGrid = true;
 };
 
-/*
+
 $scope.friends = [];
 
 $scope.friend = {};
@@ -38,7 +59,22 @@ $scope.add = function(){
 	$log.info($scope.friend);
 	$scope.friends.push($scope.friend);
 	$scope.friend = {};
-};*/
+};
+
+
+
+ $scope.deleteClicked = function(friend) {
+	$log.info(friend);
+	var index = $scope.friends.indexOf(friend);
+	$scope.friends.splice(index, 1);
+  };
+
+$scope.editClicked = function(friend) {
+	$log.info(friend);
+	friend.name = $scope.friend.name;
+	friend.lives = $scope.friend.lives;
+  };
+
 
 
 });
