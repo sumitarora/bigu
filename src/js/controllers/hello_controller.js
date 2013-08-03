6538f97dbc51fe5002dc608e5613c4cf4879a023
Meenu Bhagat <meeeenubhagat87@gmail.com>
2013-08-04 01:37:12 +0530

    :)

--- src/js/controllers/hello_controller.js
+++ src/js/controllers/hello_controller.js
@@ -1,8 +1,20 @@
 angular.module('bigu.controllers').controller('HelloCtrl', function ($scope, $log) {
 
+	
+
+
 $log.info("controller");
 
   
+  $scope.friends = [
+     { name:'Gulabo', lives:'Canada'},
+       {name:'Bilo', lives:'India'}];
+
+
+$scope.Click = function(){
+	$scope.friends.push({name:'XYZ', lives:'Lives'});
+
+};
   
 
 });
