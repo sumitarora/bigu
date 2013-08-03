90152d96f3c8cc160dc9e1d6e410dca952cf2606
Meenu Bhagat <meeeenubhagat87@gmail.com>
2013-08-04 00:53:33 +0530

    "changes"

--- src/js/controllers/categories_controller.js
+++ src/js/controllers/categories_controller.js
@@ -1,7 +1,15 @@
 angular.module('bigu.controllers').controller('CategoriesCtrl', function ($scope, $log) {
 
-$log.info("Categories controller initiated");
+$log.info("Categories controller initiated now");
 $scope.showGrid = true;
+
+$scope.addClicked = function(){
+	$scope.showGrid = false;
+};
+
+$scope.cancelClicked = function(){
+	$scope.showGrid = true;
+};
   
   
 
