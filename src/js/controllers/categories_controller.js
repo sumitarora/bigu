de251de96921edb2e169e7c37d8b89cfa8f17d98
Meenu Bhagat <meeeenubhagat87@gmail.com>
2013-08-05 23:12:54 +0530

    all task done with help

--- src/js/controllers/categories_controller.js
+++ src/js/controllers/categories_controller.js
@@ -3,26 +3,60 @@ angular.module('bigu.controllers').controller('CategoriesCtrl', function ($scope
 $log.info("Categories controller initiated now");
 $scope.showGrid = true;
 
+$scope.addClicked = function(){
+	$log.info("Add Clicked");
+	$scope.showGrid = false;
+	
+};
 
 $scope.categories = [
-{name:"categories1", title:"Asha title"},
-{name:"categories2", title:"Sumit Arora title"}];
+{title:"Title ", parent:" Parent ",type:" Type"},
+{title:"PHP ", parent:" main ",type:" Programming language"},
+{title:"JAVA ", parent:" web ",type:" Programming language "},
+{title:"Cobal ", parent:" website ",type:" Programming language "},
+{title:"udmey ", parent:" main ",type:" Programming language "}];
+
+
+
 
 $scope.addClicked = function(){
-	$scope.categories.push({name:"Meenu", title:"Denik bhaskar"});
-	/*
+	/*$scope.categories.push({title:"Programming Language" , type:"main" ,parent:"web"});*/
 	$scope.showGrid = false;
-	$log.info($scope.friend);
-	$scope.friends.push($scope.friend);
-	$scope.friend = {};*/
+	$scope.category = {
 
+};
+};
+
+$scope.saveClicked = function(){
+
+if($scope.index > -1){
+  $scope.categories[$scope.index] = $scope.category;
+  $scope.index = -1;
+  }
+  else{
+  $scope.categories.push($scope.category);
+  }
+  $scope.showGrid = true;
 };
 
 $scope.cancelClicked = function(){
 	$scope.showGrid = true;
 };
   
+$scope.deleteClicked = function(category){
+	$log.info("delete category",category);
+	var index = $scope.categories.indexOf(category);
+	$scope.categories.splice(index, 1);
 
+
+};
+$scope.editClicked = function(category){
+	$log.info(category);
+	$scope.category = angular.copy(category);
+	$scope.showGrid = false;
+	$scope.index = $scope.categories.indexOf(category);
+
+};
   
 
 });
