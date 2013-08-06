19e74e26f1159b9ead255de90a43e635e7b25f07
Meenu Bhagat <meeeenubhagat87@gmail.com>
2013-08-07 01:09:41 +0530

    task 6

--- src/js/controllers/user_controller.js
+++ src/js/controllers/user_controller.js
@@ -4,6 +4,20 @@ angular.module('bigu.controllers').controller('UserCtrl', function ($scope, $log
 
 
 $log.info(" This is User controller ");
+	$scope.showGrid = true;
 
+	$scope.addClicked = function(){
+		$log.info("Adding Detail");
+		$scope.showGrid = false;
+	};
+
+	$scope.users = [
+	{email:"meenubhagat91@yahoo.com" ,FName:"Meenu",LName:"Bhagat",Profession:"Student",Country:"India"}];
+
+	$scope.cancelClicked = function(){
+		$scope.showGrid = true;
+	};
+
+	
 
   });
