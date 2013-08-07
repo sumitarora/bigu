c85d745654931d0335e7375645b82ee9e0ca422f
Meenu Bhagat <meeeenubhagat87@gmail.com>
2013-08-08 00:13:14 +0530

    task 6 done

--- src/js/controllers/user_controller.js
+++ src/js/controllers/user_controller.js
@@ -1,8 +1,5 @@
 angular.module('bigu.controllers').controller('UserCtrl', function ($scope, $log) {
 
-	
-
-
 $log.info(" This is User controller ");
 	$scope.showGrid = true;
 
@@ -12,10 +9,10 @@ $log.info(" This is User controller ");
 	};
 
 	$scope.users = [
-	{email:"meenubhagat91@yahoo.com" ,FName:"Meenu",LName:"Bhagat",profession:"Student",country:"India",facebook:"fb@facebook.com"},
-	{email:"rubyverma@yahoo.com" ,FName:"Ruby",LName:"verma",profession:"Student",country:"Canada",facebook:"fb@facebook.com"},
-	{email:"Arorasumit91@yahoo.com" ,FName:"Sumit",LName:"Arora",profession:"Student",country:"Canada",facebook:"fb@facebook.com"},
-	{email:"sbhagat@yahoo.com" ,FName:"Sonia",LName:"Bhagat",profession:"Student",country:"India",facebook:"fb@facebook.com"}];
+	{email:"meenubhagat91@yahoo.com" ,FName:"Meenu",LName:"Bhagat",profession:"Student",country:"India",facebook:"fb@facebook.com",twitter:"mfjn@twitter.com"},
+	{email:"rubyverma@yahoo.com" ,FName:"Ruby",LName:"verma",profession:"Student",country:"Canada",facebook:"fb@facebook.com",twitter:"jhjh@twitter.com"},
+	{email:"Arorasumit91@yahoo.com" ,FName:"Sumit",LName:"Arora",profession:"Student",country:"Canada",facebook:"fb@facebook.com",twitter:"hyg@twitter.com"},
+	{email:"sbhagat@yahoo.com" ,FName:"Sonia",LName:"Bhagat",profession:"Student",country:"India",facebook:"fb@facebook.com",twitter:"kjgug@twitter.com"}];
 
 	$scope.addClicked = function(){
 		$scope.showGrid = false;
@@ -36,6 +33,21 @@ $log.info(" This is User controller ");
 		$scope.showGrid = true;
 
 	};
+
+	$scope.deleteClicked = function(duser){
+	$log.info("delete category", duser);
+	var index = $scope.users.indexOf(duser);
+	$scope.users.splice(index, 1);
+
+
+};
+$scope.editClicked = function(duser){
+	$log.info(duser);
+	$scope.duser = angular.copy(duser);
+	$scope.showGrid = false;
+	$scope.index = $scope.categories.indexOf(duser);
+
+};
 
 	$scope.cancelClicked = function(){
 		$scope.showGrid = true;
