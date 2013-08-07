8e5f91593bf0dceca9a90acdb952fd07cae46ead
Meenu Bhagat <meeeenubhagat87@gmail.com>
2013-08-07 23:20:02 +0530

    :) :)

--- src/js/controllers/user_controller.js
+++ src/js/controllers/user_controller.js
@@ -12,7 +12,30 @@ $log.info(" This is User controller ");
 	};
 
 	$scope.users = [
-	{email:"meenubhagat91@yahoo.com" ,FName:"Meenu",LName:"Bhagat",Profession:"Student",Country:"India"}];
+	{email:"meenubhagat91@yahoo.com" ,FName:"Meenu",LName:"Bhagat",profession:"Student",country:"India",facebook:"fb@facebook.com"},
+	{email:"rubyverma@yahoo.com" ,FName:"Ruby",LName:"verma",profession:"Student",country:"Canada",facebook:"fb@facebook.com"},
+	{email:"Arorasumit91@yahoo.com" ,FName:"Sumit",LName:"Arora",profession:"Student",country:"Canada",facebook:"fb@facebook.com"},
+	{email:"sbhagat@yahoo.com" ,FName:"Sonia",LName:"Bhagat",profession:"Student",country:"India",facebook:"fb@facebook.com"}];
+
+	$scope.addClicked = function(){
+		$scope.showGrid = false;
+		$scope.duser = {
+
+		};
+
+	};
+	$scope.saveClicked = function(){
+		$log.info("Add new detail")
+		if($scope.index > -1){
+			$scope.users[$scope.index] = $scope.duser;
+			$scope.index = -1;
+		}
+		else{
+			$scope.users.push($scope.duser);
+		}
+		$scope.showGrid = true;
+
+	};
 
 	$scope.cancelClicked = function(){
 		$scope.showGrid = true;
